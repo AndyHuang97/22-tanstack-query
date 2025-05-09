980edda6669bf32c6d1f4a358d980d6fff148f4b
Andrea Huang <huangandrea@hotmail.it>
2025-05-09 18:14:21 +0200

    React Router & Tanstack Query synergy

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,54 +1,68 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useNavigation,
+  useParams,
+  useSubmit,
+} from "react-router-dom";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { state } = useNavigation();
+  const submit = useSubmit();
   const params = useParams();
 
-  const { data, isPending, isError, error } = useQuery({
+  // with loader there is no need for isPending, page is rendered after data is received
+  const { data, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+    staleTime: 5000, // use cache data (avoid additional http request)
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async (mutateData) => {
-      // data passed to mutate()
-      const newEvent = mutateData.event;
-      console.log(newEvent);
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async (mutateData) => {
+  //     // data passed to mutate()
+  //     const newEvent = mutateData.event;
+  //     console.log(newEvent);
 
-      // abort ongoing queries with given queryKey
-      console.log("start aborting");
-      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
-      console.log("end aborting");
-      const previousEvent = queryClient.getQueryData(["events", params.id]);
+  //     // abort ongoing queries with given queryKey
+  //     console.log("start aborting");
+  //     await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+  //     console.log("end aborting");
+  //     const previousEvent = queryClient.getQueryData(["events", params.id]);
 
-      // force explicitly the data before http response is returned
-      queryClient.setQueryData(["events", params.id], newEvent);
+  //     // force explicitly the data before http response is returned
+  //     queryClient.setQueryData(["events", params.id], newEvent);
 
-      // return the context
-      return { previousEvent };
-    },
-    onError: (error, data, context) => {
-      // rollback
-      queryClient.setQueryData(["events", params.id], context.previousEvent);
-      console.error("Update failed:", error);
-    },
-    onSettled: () => {
-      // after either error or success, synchronize state with backend
-      queryClient.invalidateQueries(["events", params.id]);
-    },
-  });
+  //     // return the context
+  //     return { previousEvent };
+  //   },
+  //   onError: (error, data, context) => {
+  //     // rollback
+  //     queryClient.setQueryData(["events", params.id], context.previousEvent);
+  //     console.error("Update failed:", error);
+  //   },
+  //   onSettled: () => {
+  //     // after either error or success, synchronize state with backend
+  //     queryClient.invalidateQueries(["events", params.id]);
+  //   },
+  // });
 
   function handleSubmit(formData) {
-    mutate({ id: params.id, event: formData });
-    navigate("../");
+    // mutate({ id: params.id, event: formData });
+    // navigate("../");
+
+    // any method different from "GET"
+    // this method does not trigger an http request, but the action
+    submit(formData, { method: "PUT" });
   }
 
   function handleClose() {
@@ -56,14 +70,6 @@ export default function EditEvent() {
   }
 
   let content;
-
-  if (isPending) {
-    content = (
-      <div className='center'>
-        <LoadingIndicator />
-      </div>
-    );
-  }
 
   if (isError) {
     content = (
@@ -84,15 +90,45 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to='../' className='button-text'>
-          Cancel
-        </Link>
-        <button type='submit' className='button'>
-          Update
-        </button>
+        {state === "submitting" ? (
+          <p>Sending data</p>
+        ) : (
+          <>
+            <Link to='../' className='button-text'>
+              Cancel
+            </Link>
+            <button type='submit' className='button'>
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
 
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+// Using React Router's loader does not make the useQuery useless
+// It's best used in conjunction with useQuery, the loader will put the data in
+// cache, and subsequent operations on the page will leverage the cache capabilities
+// of Tanstack query
+export function loader({ params }) {
+  // trigger query programmatically outside React components
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  });
+}
+
+// form submission for simple cases
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+
+  await updateEvent({ id: params.id, event: updatedEventData });
+
+  // no more optimistic update
+  await queryClient.invalidateQueries(["events"]);
+  return redirect("../");
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
+import { useIsFetching } from "@tanstack/react-query";
+
 export default function Header({ children }) {
+  // 0 if not fetching
+  // > 0 if fetching
+  const fetching = useIsFetching();
+
   return (
     <>
-      <div id="main-header-loading"></div>
-      <header id="main-header">
-        <div id="header-title">
+      <div id='main-header-loading'>{fetching > 0 && <progress />}</div>
+      <header id='main-header'>
+        <div id='header-title'>
           <h1>React Events</h1>
         </div>
         <nav>{children}</nav>
