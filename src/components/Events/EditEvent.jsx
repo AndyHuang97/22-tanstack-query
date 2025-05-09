d754c68f338b7e6d21f997ef5c66f36c1cd3d156
Andrea Huang <huangandrea@hotmail.it>
2025-05-09 15:59:04 +0200

    Updating Data with Mutations (WIP)

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,10 +1,10 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
-import { fetchEvent } from "../../util/http.js";
+import { fetchEvent, updateEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
@@ -16,7 +16,14 @@ export default function EditEvent() {
     queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
   });
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate("../");
+  }
 
   function handleClose() {
     navigate("../");
@@ -26,7 +33,7 @@ export default function EditEvent() {
 
   if (isPending) {
     content = (
-      <div className="center">
+      <div className='center'>
         <LoadingIndicator />
       </div>
     );
